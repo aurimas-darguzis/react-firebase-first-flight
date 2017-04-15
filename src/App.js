b69cc9b19acc8bc8f6622a1e7ec023cb12e9109d
aurimas-darguzis <aurimas@darguzis.com>
2017-04-15 23:28:22 +0100

    set up with firebase web

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App">
-        <div className="App--header">
+      <div className='App'>
+        <div className='App--header'>
           <h2>Welcome to React and Firebase</h2>
         </div>
-        <pre className="App--data">
+        <pre className='App--data'>
           One day, some data from Firebase will go here.
         </pre>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
